674968dcb0fe8129a04ea32802d83a32297aeb28
Karim Alomari <karim.alomari@gmail.com>
2020-07-27 14:48:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 katz_deli = []
 
+
 def line(line_array)
   if line_array.size == 0 
     puts "The line is currently empty."
@@ -11,6 +12,7 @@ def line(line_array)
   puts line_message.rstrip
   end
 end
+
 
 def take_a_number(line_array, name)
   line_array << name
