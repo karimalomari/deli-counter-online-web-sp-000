9cb99d06c4fb0a869c8c836e35f2e921ce9d576c
Karim Alomari <karim.alomari@gmail.com>
2020-07-27 14:49:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,5 +21,9 @@ end
 
 
 def now_serving(line_array)
-  
+  if line_array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current_customer = line_array.shift
+    puts "Currently serving #{current_customer}."
 end
